970da1b61d9523cc76b11301d1d9cb2e64d1cd07
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-14 13:21:00 +0900

    Add validates in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :email, :encrypted_password, :familyname, :name, :kana_familyname, :kana_name, :birthday ,presence: true
 end
