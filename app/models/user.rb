3dbd24997914b4dacb0e6bc224b1e989664b5325
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-14 18:24:11 +0900

    Change validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :email, :encrypted_password, :familyname, :name, :kana_familyname, :kana_name, :birthday ,presence: true
-  #メールアドレスのフォーマットを指定
+  # メールアドレスのフォーマットを指定
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  #名前の入力を全角のみに指定
+  # 名前の入力を全角のみに指定
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
-  validates :familyname, :name, :kana_familyname, :kana_name, format: { with: VALID_NAME_REGEX }
+  validates :familyname, :name, format: { with: VALID_NAME_REGEX }
+  # かなの入力を平仮名全角のみに指定
+  VALID_KANANAME_REGEX = /\A[ぁ-ん]+\z/
+  validates :kana_familyname, :kana_name, format: { with: VALID_KANANAME_REGEX }
 end
