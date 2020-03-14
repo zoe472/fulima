4f7f87eeccedad5df830d0c3b90634acbd1375e3
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-14 13:14:38 +0900

    Add configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth,  if: :production? 
   protect_from_forgery with: :exception
 
+  #deviseのカラムを追加したため、カラムを承認するための記述
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :familyname, :name, :kana_familyname, :kana_name, :birthday])
+  end
+
   private
 
   def production?
