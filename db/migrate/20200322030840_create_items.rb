912fe0ceef06d9e5db1ae1c2fe82555e5d3dbab4
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-03-22 12:17:14 +0900

    Update 20200322030840_create_items.rb

--- db/migrate/20200322030840_create_items.rb
+++ db/migrate/20200322030840_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name,        null: false
+      t.text :description,   null: false
+      t.string :size,        null: false
+      t.string :status,      null: false
+      t.integer :charge,     null: false
+      t.string :region,      null: false
+      t.integer :price,      null: false
+      t.integer :date,       null: false
+      t.integer :seller_id,  foreign_key: true
+      t.integer :buyer_id,   foreign_key: true
+      t.integer :brand_id,   foreign_key: true
       t.timestamps
     end
   end
