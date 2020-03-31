ef14dbfbcee815d2b829bfe7e7120dae9448f1c5
zoe472 <seiya1387@gmail.com>
2020-04-01 00:52:24 +0900

    Update 20200323122644_create_cards.rb

--- db/migrate/20200323122644_create_cards.rb
+++ db/migrate/20200323122644_create_cards.rb
@@ -1,7 +1,7 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-      t.references :user_id, foreign_key: true, null: false
+      t.integer :user_id, foreign_key: true, null: false
       t.string :customer_id, null: false
       t.string :card_id, null: false
       t.timestamps
