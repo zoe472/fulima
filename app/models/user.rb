f540ddd4e05f5091b483d6910c4174dd67be4858
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-01 10:37:06 +0900

    Add error-message

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   validates :nickname, :email, :encrypted_password, :familyname, :name, :kana_familyname, :kana_name, :birthday ,presence: true
   # メールアドレスのフォーマットを指定
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "は「aaa@bbb.ccc」の形式で入力してください" }
   # 名前の入力を全角のみに指定
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
-  validates :familyname, :name, format: { with: VALID_NAME_REGEX }
+  validates :familyname, :name, format: { with: VALID_NAME_REGEX, message: "は全角で入力してください" }
   # かなの入力を平仮名全角のみに指定
   VALID_KANANAME_REGEX = /\A[ぁ-ん]+\z/
-  validates :kana_familyname, :kana_name, format: { with: VALID_KANANAME_REGEX }
+  validates :kana_familyname, :kana_name, format: { with: VALID_KANANAME_REGEX, message: "は全角平仮名で入力してください" }
 
   has_one :address
   has_many :cards, dependent: :destroy
