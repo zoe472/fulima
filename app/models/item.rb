d176c60b1c76cecd26cd4a6b1f38df6d11b27564
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-02 11:37:03 +0900

    Add association of brand

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
 
   belongs_to :seller, foreign_key: "seller_id", class_name: "User"
   belongs_to :buyer, foreign_key: "buyer_id", class_name: "User"
+  belongs_to :brand
   has_many :goods
   has_many :comments
   has_many :pictures
