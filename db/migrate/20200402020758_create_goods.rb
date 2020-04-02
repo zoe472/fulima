7b4f9ca38419283719cbedc74a4fed7b881873e6
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-02 11:11:57 +0900

    rails db:migrate

--- db/migrate/20200402020758_create_goods.rb
+++ db/migrate/20200402020758_create_goods.rb
@@ -1,6 +1,8 @@
 class CreateGoods < ActiveRecord::Migration[5.2]
   def change
     create_table :goods do |t|
+      t.integer :user_id, foreign_key: true
+      t.integer :item_id, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_091016) do
+ActiveRecord::Schema.define(version: 2020_04_02_020758) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "familyname", null: false
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_04_01_091016) do
     t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
