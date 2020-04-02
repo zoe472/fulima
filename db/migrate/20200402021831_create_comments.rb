98a9530404b0e01f76a4904b3fffa02080edd46f
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-02 11:22:49 +0900

    rails db:migrate

--- db/migrate/20200402021831_create_comments.rb
+++ db/migrate/20200402021831_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.text :sentence, null: false
+      t.integer :user_id, foreign_key: true, index: true
+      t.integer :item_id, foreign_key: true, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_020758) do
+ActiveRecord::Schema.define(version: 2020_04_02_021831) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "familyname", null: false
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2020_04_02_020758) do
     t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "sentence", null: false
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
