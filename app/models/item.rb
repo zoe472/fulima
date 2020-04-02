cbf4b844f98e91b57ed7a6a9e734201d13a4fdc1
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-02 11:14:13 +0900

    Add association of good

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
 
   belongs_to :seller, foreign_key: "seller_id", class_name: "User"
   belongs_to :buyer, foreign_key: "buyer_id", class_name: "User"
+  has_many :goods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,5 @@ class User < ApplicationRecord
   has_many :bought_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :selling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
+  has_many :goods
 end
