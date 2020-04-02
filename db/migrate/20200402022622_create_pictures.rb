d06448e9f0fc8350e2371b01591024e145142a49
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-02 11:28:53 +0900

    rails db:migrate

--- db/migrate/20200402022622_create_pictures.rb
+++ db/migrate/20200402022622_create_pictures.rb
@@ -1,6 +1,7 @@
 class CreatePictures < ActiveRecord::Migration[5.2]
   def change
     create_table :pictures do |t|
+      t.string :image, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_021831) do
+ActiveRecord::Schema.define(version: 2020_04_02_022622) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "familyname", null: false
@@ -66,6 +66,12 @@ ActiveRecord::Schema.define(version: 2020_04_02_021831) do
     t.integer "seller_id"
     t.integer "buyer_id"
     t.integer "brand_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
