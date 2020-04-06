9c44ddb22837181ec155a5f92d75e4b834298d83
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-04-06 10:14:57 +0900

    Delete paginate in topindex

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   def index
-    @items = Item.includes(:seller).order("created_at DESC").page(params[:page]).per(3)
+    @items = Item.includes(:seller).order("created_at DESC").limit(3)
     @random = Item.includes(:seller).order("RAND()").limit(3)
   end
 
