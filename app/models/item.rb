7937a1cb16ba3cb484b03a1ab5fdae9133835963
zoe472 <seiya1387@gmail.com>
2020-04-14 21:03:13 +0900

    items-detail

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,12 @@ class Item < ApplicationRecord
   has_many :pictures
   has_many :category_items
   has_many :categories, through: :category_items
+  
+  def previous
+    Item.where("id < ?",self.id).order("id DESC").first
+  end
+
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
 end
