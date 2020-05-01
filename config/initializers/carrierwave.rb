6724aac078ddcdde59d0a84cc43a060680dcf13e
zoe472 <seiya1387@gmail.com>
2020-05-01 20:04:03 +0900

    aa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
     }
 
     config.fog_directory  = 'fulima'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fulima'
+    config.asset_host = 'https://fulima.s3.amazonaws.com'
 end
