258fe64dde6ab35f9237dd09ba07aaec93bb2c22
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-05-02 20:59:28 +0900

    Add pictures validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,4 @@
 class Item < ApplicationRecord
-  
-  validates :name, :description, :size, :charge, :status, :region, :price, :date, :seller_id, presence: true
-
-  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
-
-
   belongs_to :seller, foreign_key: "seller_id", class_name: "User"
   belongs_to :buyer, foreign_key: "buyer_id", class_name: "User", optional: true
   has_many :goods
@@ -13,7 +7,12 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :pictures, allow_destroy: true
   has_many :category_items
   has_many :categories, through: :category_items
-  
+
+  validates :name, :description, :size, :charge, :status, :region, :price, :date, :seller_id, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
+  validates_associated :pictures
+  validates :pictures, presence: { message: 'は１枚以上５枚以内必要です' }
+
   def previous
     Item.find_by("id < ?",self.id)
   end
