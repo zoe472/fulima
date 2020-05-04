4773e16e77204661aafb177c43832458fed1017c
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-05-04 10:13:40 +0900

    Add move_to_index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:sample, :sample2, :purchace, :destroy, :edit, :update]
+  before_action :move_to_index, only: [:new, :create, :edit, :update, :destroy, :purchace]
  
   
   def index
@@ -101,6 +102,10 @@ private
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
