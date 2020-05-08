68eae80d9456c872327b411c66c85c6650f27764
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-05-08 21:14:05 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
     if @item.update(item_update_params)
       redirect_to root_path
     else
-      redirect_to edit_item_path(@item)
+      render :edit
     end
   end
 
